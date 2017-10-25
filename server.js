dd9b3a9ce40245f200ed40ba979cb9a82f5b1a0f
brickatyourfeet <brickatyourfeet@gmail.com>
2017-10-25 14:07:13 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,57 @@
+const express = require('express')
+const hbs = require('hbs')
+const fs = require('fs')
+const port = process.env.PORT || 3000
+let app = express()
+
+hbs.registerPartials(__dirname + '/views/partials')
+
+app.set('view engine', 'hbs')
+app.use(express.static(__dirname + '/public'))
+
+app.use((req, res, next) => {
+  let now = new Date().toString()
+  let log = `${now}: ${req.method} ${req.url}`
+
+  console.log(log)
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if (err) {
+      console.log('Unable to append to server.log.')
+    }
+  })
+  next()
+})
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear()
+})
+
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase()
+})
+
+app.get('/', (req, res) => {
+  //res.send('<h1>This is express</h1>')
+  res.render('home.hbs', {
+    pageTitle: 'Home Page',
+    welcomeMessage: 'Welcome to my website',
+  })
+})
+
+app.get('/about', (req, res) => {
+  res.render('about.hbs', {
+    pageTitle: 'About Page',
+  })
+
+})
+
+app.get('/bad', (req, res) => {
+  //res.send('<h1>This is express</h1>')
+  res.send({
+    errorMessage: 'page not found or something'
+  })
+})
+
+app.listen(port, () => {
+  console.log(`server up and running on ${port}`)
+})
